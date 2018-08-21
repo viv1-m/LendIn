ff15cafeff070c2593c849c45d4a18c4027460e1
viv1-m <vivek1225m@gmail.com>
2018-08-22 00:46:04 +0530

    fff

--- app/src/main/java/com/example/dinesh/myapplication/Add_transaction.java
+++ app/src/main/java/com/example/dinesh/myapplication/Add_transaction.java
@@ -12,11 +12,27 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import sun.rmi.runtime.Log;
+
 public class Add_transaction extends AppCompatActivity {
 
     EditText amt;
     Spinner spinner;
     Button button;
+
+    ArrayList<String> names = new ArrayList<String>();
+
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, names);
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference().child("users");
+
+    FirebaseUser user = mAuth.getCurrentUser();
+
+    String uid = user.getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +59,21 @@ public class Add_transaction extends AppCompatActivity {
                     }
                 });
     }
+
+    myRef.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // This method is called once with the initial value and again
+            // whenever data at this location is updated.
+            Log.d("TAG", "User name is " + (String) dataSnapshot.child("FN6sV61ej6gN4eYn3cQkmbhzrP93").child("name").getValue());
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError error) {
+            // Failed to read value
+            Log.w("TAG", "Failed to read value.", error.toException());
+        }
+    });
 
 }
